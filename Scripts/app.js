dc207df41cf7e1c7759f1fd38c77228358e55332
ChadClark_Dev <chadclarke@bell.net>
2016-01-28 10:54:31 -0500

    first commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,24 @@
-console.log("App started...");
+// main java document 
+// array stores data 
+
+"use strict"; //show errors 
+// variables 
+var username = "Chad" ; 
+var age = 30; 
+// boolean example 
+var isHungry = true; 
+
+// array 
+var usernames = ["mike", "rich", "margo","chad"];
+//log to console 3rd element (margo) 
+console.log(usernames);
+
+//add element to bottom 
+usernames.push("Matt");
+// print 
+console.log("After the push:" + usernames);
+
+
+//remove element from 
+usernames.pop();
+console.log("After the pop:" + usernames);
